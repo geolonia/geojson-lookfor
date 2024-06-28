9a1332916024d2599dc0acb92ab34d2738d33a3a
piguchi@geolonia.com <piguchi@geolonia.com>
2024-06-28 22:34:51 +0900

    lookfor関数を作成

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 
-export default class GeoJsonLookup {
+export default class GeoJsonlookfor {
+
+    geojson: any;
+    
+    constructor(geojson: any) {
+        this.geojson = geojson;
+    }
+
+    // "keyword"を含む項目があるfeatureを検索する
+    lookfor(keyword: string) {
+        const features = this.geojson.features;
+        const res = {
+            "type": "FeatureCollection",
+            "features": features.filter(
+                (feature: any) => JSON.stringify(feature).includes(keyword)
+            )
+        };
+        console.log(res);
+        return res;
+    }
     
 }
