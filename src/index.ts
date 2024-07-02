1614ba86abdee46765b011fb35f62180cbd05d31
piguchi@geolonia.com <piguchi@geolonia.com>
2024-07-02 14:11:08 +0900

    インデントの修正、返すエラー内容を修正

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
 
 export default class GeoJsonlookfor {
 
-    geojson: any;
-    
-    constructor(geojson: any) {
-        this.geojson = geojson;
-    }
+  geojson: any;
+  
+  constructor(geojson: any) {
+    this.geojson = geojson;
+  }
 
-    // "keyword"を含む項目があるfeatureを検索する
-    lookfor(keyword: string) {
-      try {
-          if (this.geojson === undefined || this.geojson === null || typeof this.geojson !== 'object' || typeof this.geojson === 'string') {
-              throw new Error('Invalid GeoJSON');
-          }
-          const features = this.geojson.features
-
-          this.geojson = {
-              "type": "FeatureCollection",
-              "features": features.filter((feature: any) => {
-                  return JSON.stringify(feature).includes(keyword);
-              })
-          };
-          
-          return this;
-      } catch (err: any) {
-          throw err
+  // "keyword"を含む項目があるfeatureを検索する
+  lookfor(keyword: string) {
+    try {
+      if (this.geojson === undefined || this.geojson === null || typeof this.geojson !== 'object' || typeof this.geojson === 'string') {
+        throw new Error('Invalid GeoJSON');
       }
-    }
+      const features = this.geojson.features
 
-    // geojsonを返す
-    getGeoJSON() {
-        return this.geojson
+      this.geojson = {
+        "type": "FeatureCollection",
+        "features": features.filter((feature: any) => {
+          return JSON.stringify(feature).includes(keyword);
+        })
+      };
+      
+      return this;
+    } catch (err: any) {
+      throw new Error(err);
     }
+  }
+
+  // geojsonを返す
+  getGeoJSON() {
+    return this.geojson
+  }
     
 }
